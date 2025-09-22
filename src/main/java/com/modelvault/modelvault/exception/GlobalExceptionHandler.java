15a7f9c0632fa43bf85eaf3a6b467cf3d67cae43
Anmol Kumar <anmolsinghs413@gmail.com>
2025-09-22 18:14:00 +0530

    Add global exception handling

---
+++ src/main/java/com/modelvault/modelvault/exception/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.modelvault.modelvault.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Date;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<Object> handleGlobalException(Exception ex, WebRequest request) {
+		Map<String, Object> body = Map.of("timestamp", new Date(), "message", ex.getMessage());
+		return new ResponseEntity<>(body, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+}
