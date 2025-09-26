abe7ef53d6fbcbf1f83c1678aba0dcf50c69be0e
Anmol Kumar <anmolsinghs413@gmail.com>
2025-09-26 13:14:00 +0530

    Add basic application context test

---
+++ src/test/java/com/modelvault/modelvault/ModelvaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modelvault.modelvault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModelvaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
